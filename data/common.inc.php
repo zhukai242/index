b2d8f7c82340034a3dc901104664f84b58cc8f20
unknown <416965825@qq.com>
2021-08-11 20:21:27 +0800

    init

---
+++ data/common.inc.php
@@ -0,0 +1,12 @@
+<?php
+//数据库连接信息
+$cfg_dbtype = 'mysql';
+$cfg_dbhost = '192.168.1.177';
+$cfg_dbname = 'dedecmsv57utf8sp2';
+$cfg_dbuser = 'root';
+$cfg_dbpwd = '730e0a5509b6cf8c';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'utf8';
+
+
+?>
